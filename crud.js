05426079ff79cc45b834c8c5bf214230e78ef457
geoffreyduchanois <geoff.ducha@hotmail.com>
2020-12-05 15:56:02 +0100

    fonction post'

--- crud.js
+++ crud.js
@@ -2,19 +2,27 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 const sql = require('./connection.js')
+const bodyParser= require('body-parser');
+
 
 // création serveur
 app.listen(8000 ,()=> console.log('En écoute sur le port 8000'));
+
+// initialistation bodyparser
+
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.use(bodyParser.json());
+
 // GET  tout les fichier
 
 
-
 app.get(`/files/get`, (req , res)=> {
-    var donne= []
-    var result = sql().query('SELECT * FROM Refs;',(err , docs , field)=>{
+    var donne=[];
+    sql().query('SELECT * FROM Refs;',(err , docs , field)=>{
         if (err) throw err;
         for (let i=0; i< docs.length; i++){
-            donne.push(docs[i].id_reference, docs[i].Ref , docs[i].Type);
+            donne.push(docs[i].id_reference, docs[i].name , docs[i].Type);
         }
         
         res.json({
@@ -22,20 +30,28 @@ app.get(`/files/get`, (req , res)=> {
         })
      })
 });
+
+
 //  GET un seul fichier 
 app.get(`/files/get/:id`, (req, res) => {
     const id =  [req.params.id];
     var donne = []
-    var result = sql().query('SELECT * FROM Refs WHERE `id_reference`='+id, (err, docs, field) => {
+    sql().query('SELECT * FROM Refs WHERE `id_reference`='+id, (err, docs, field) => {
         if (err) throw err;
-        donne.push(docs[0].id_reference, docs[0].Ref, docs[0].Type);
+        donne.push(docs[0].id_reference, docs[0].name, docs[0].Type);
         res.json({
             data: donne
         })
     })
 });
 
-// var result = sql.query('SELECT * FROM Refs;',data,(err,user,fields));
-// if (err) throw err;
-// console.log(user);
-
+// POST 
+app.post(`/files/post`,(req,res)=>{
+    const data = req.body;
+    sql().query('INSERT INTO`Refs`(`id_reference`, `name`, `Type`) VALUES(' + data.id_reference+',\''+ data.name + '\',\'' + data.Type + '\');', (err, Refs, field) => {
+        if (err) throw err;
+        res.json({
+            truc: data.name +' a été crée'
+        })
+    })
+});
